f34bb695796fa70d59851ab46bbf1ec2956ff552
Shankar Prasad - Data Engineer <mcshankarprasad@gmail.com>
2026-07-24 22:58:16 +0530

    [dev]-spark demo

---
+++ modern-data-engineering-platform/src/main/scala/EmployeesDataFrameDemo.scala
@@ -0,0 +1,11 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object EmployeesDataFrameDemo {
+
+  def main(args:Array[String]): Unit = {
+
+
+  }
+
+}
---
+++ modern-data-engineering-platform/src/main/scala/HelloSpark.scala
@@ -0,0 +1,15 @@
+import org.apache.spark.sql.SparkSession
+
+object HelloSpark {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("spark test")
+      .master("local[*]")
+      .getOrCreate()
+    println("spark Version: " + spark.version)
+
+    spark.stop()
+
+  }
+}
